8c4e9c2814d630bb556b71b340b01627cbc0b9e9
Jesus Gutierrez <jesus.gutierrez@sewan.es>
2025-04-03 11:33:41 +0200

    Fin seccion 9

---
+++ DB/config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_CNN);
+
+        console.log('Base de datos online');
+
+    }catch(err){
+        console.log(err);
+        throw new Error('Error iniciatizar base de datos');
+    }
+
+}
+
+module.exports = {
+    dbConnection
+}
